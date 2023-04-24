025120de8eb75d1816b3a331c6a43bb86a614ed9
natalia-ya <sagaidak.nata@gmail.com>
2023-04-24 14:32:37 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,10 @@
 async function getPokemonData() {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-  const data = await response.json();
-  console.log(data);
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/6")
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((json) => console.log(json))
+    .catch((error) => console.error(error));
 }
 getPokemonData();
